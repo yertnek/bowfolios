05fe9860c34d520db4a9f6458a4696ec64a722a1
trey-sumida <54917302+trey-sumida@users.noreply.github.com>
2020-08-27 20:11:59 -1000

    Added profiles, projects, interests
    The tabs work however the projects and interests pages havent been created yet so they all route to the profiles page.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,8 +2,28 @@ import 'package:flutter/material.dart';
 
 import 'package:bowfolios/widgets/main_drawer.dart';
 import 'package:bowfolios/screens/profiles_screen.dart';
+import 'package:bowfolios/screens/loading_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<Widget> _pages = [
+    ProfilesScreen(),
+    ProfilesScreen(),
+    ProfilesScreen(),
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,8 +31,27 @@ class HomeScreen extends StatelessWidget {
         title: Text('BowFolios'),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: ProfilesScreen(),
+      body: _pages[_selectedPageIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Colors.white,
+        currentIndex: _selectedPageIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            title: Text('Profiles'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.build),
+            title: Text('Projects'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            title: Text('Interests'),
+          ),
+        ],
       ),
     );
   }
