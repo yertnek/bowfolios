4fbbc9faa0a795d46bc2861bc8228d610ffc480b
trey-sumida <54917302+trey-sumida@users.noreply.github.com>
2020-08-31 22:11:04 -1000

    added interests and image section on projects card

--- lib/widgets/project_card.dart
+++ lib/widgets/project_card.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 class ProjectCard extends StatelessWidget {
   final String title;
   final String desc;
+  final List<String> interests;
 
-  ProjectCard(this.title, this.desc);
+  ProjectCard(this.title, this.desc, this.interests);
 
   @override
   Widget build(BuildContext context) {
@@ -16,8 +17,50 @@ class ProjectCard extends StatelessWidget {
             title: Text(title),
           ),
           Container(
-            padding: EdgeInsets.only(bottom: 10, left: 10, right: 10),
+            padding: EdgeInsets.only(
+              left: 10,
+              right: 10,
+              bottom: 10,
+            ),
             child: Text(desc),
+          ),
+          Divider(
+            indent: 10,
+            endIndent: 10,
+            height: 0,
+            color: Colors.black,
+          ),
+          ButtonBar(
+            alignment: MainAxisAlignment.start,
+            children: [
+              for (var interest in interests)
+                RaisedButton(
+                  disabledColor: Colors.teal,
+                  child: Text(
+                    interest,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+            ],
+          ),
+          Divider(
+            indent: 10,
+            endIndent: 10,
+            height: 0,
+            color: Colors.black,
+          ),
+          Row(
+            children: [
+              Container(
+                padding: EdgeInsets.all(10),
+                child: Image(
+                  height: 50,
+                  width: 50,
+                  image: NetworkImage(
+                      'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                ),
+              ),
+            ],
           ),
         ],
       ),
--- lib/widgets/projects.dart
+++ lib/widgets/projects.dart
@@ -8,10 +8,16 @@ class Projects extends StatelessWidget {
     return ListView(
       padding: EdgeInsets.all(10),
       children: <Widget>[
-        ProjectCard('Open Power Quality',
-            'Open source hardware and software for distributed power quality data collection, analysis, and visualization.'),
-        ProjectCard('RadGrad',
-            'Growing awesome computer scientists, one graduate at a time.'),
+        ProjectCard(
+          'Open Power Quality',
+          'Open source hardware and software for distributed power quality data collection, analysis, and visualization.',
+          ['Software Development', 'Web Development'],
+        ),
+        ProjectCard(
+          'RadGrad',
+          'Growing awesome computer scientists, one graduate at a time.',
+          ['Software Development', 'Web Development'],
+        ),
       ],
     );
   }
