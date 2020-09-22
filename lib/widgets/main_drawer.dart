7bc71e32f49ce29f6d01d62edefca3bef3da360e
trey-sumida <54917302+trey-sumida@users.noreply.github.com>
2020-09-22 13:58:46 -1000

    Other buttons arent displayed when logged out

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import '../screens/auth_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../screens/auth_screen.dart';
+import 'package:bowfolios/screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -45,48 +48,71 @@ class _MainDrawerState extends State<MainDrawer> {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      child: Column(
-        children: <Widget>[
-          Container(
-            height: 100,
-            width: double.infinity,
-            padding: EdgeInsets.all(20),
-            alignment: Alignment.bottomLeft,
-            color: Theme.of(context).primaryColor,
-            child: Text(
-              'BowFolios',
-              style: TextStyle(
-                fontWeight: FontWeight.w900,
-                fontSize: 30,
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          buildListTile('Home', Icons.home, () {}),
-          buildListTile('Profile', Icons.person, () {}),
-          buildListTile('Add Project', Icons.add, () {}),
-          buildListTile('Search', Icons.search, () {}),
-          StreamBuilder(
-              stream: FirebaseAuth.instance.authStateChanges(),
-              builder: (ctx, userSnapshot) {
-                //if there is user data, goes to home screen
-                if (userSnapshot.hasData) {
-                  return buildListTile('Logout', Icons.exit_to_app, () {
-                    FirebaseAuth.instance.signOut();
-                  });
-                }
-                //goes to auth screen if no user data/login
-                return buildListTile('Login', Icons.exit_to_app, () {
-                  login(context);
-                });
-              }),
-        ],
+  void logout(BuildContext ctx) {
+    FirebaseAuth.instance.signOut();
+    Navigator.of(ctx).pushReplacement(
+      MaterialPageRoute(
+        builder: (_) {
+          return HomeScreen();
+        },
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (FirebaseAuth.instance.currentUser != null) {
+      return Drawer(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 100,
+              width: double.infinity,
+              padding: EdgeInsets.all(20),
+              alignment: Alignment.bottomLeft,
+              color: Theme.of(context).primaryColor,
+              child: Text('Hello'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            buildListTile('Home', Icons.home, () {}),
+            buildListTile('Profile', Icons.person, () {}),
+            buildListTile('Add Project', Icons.add, () {}),
+            buildListTile('Search', Icons.search, () {}),
+            buildListTile('Logout', Icons.exit_to_app, () {
+              logout(context);
+            }),
+          ],
+        ),
+      );
+    } else {
+      return Drawer(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 100,
+              width: double.infinity,
+              padding: EdgeInsets.all(20),
+              alignment: Alignment.bottomLeft,
+              color: Theme.of(context).primaryColor,
+              child: Text(
+                'BowFolios',
+                style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            buildListTile('Login', Icons.exit_to_app, () {
+              login(context);
+            }),
+          ],
+        ),
+      );
+    }
   }
 }
